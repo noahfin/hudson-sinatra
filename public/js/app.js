226f739cddb0ec4d1f6819cd0a7a63a5203c600b
Noah Finnerman <nonothetoad@gmail.com>
2016-04-22 13:35:46 -0400

    Change to flat ui design

--- public/js/app.js
+++ public/js/app.js
@@ -1,27 +1,7 @@
 "use strict";
 (function () {
 	console.log("js is loaded")
-$.post('place', { type: "art_gallery", field2 : "hello2"}, 
-  function(json){
-		$.each(json.results, function( index, value ) {
-   		console.log(value.name);
-   		console.log (value.geometry.location.lat)
-   		console.log(value.geometry.location.lng)
-   		  var $ul = $('<ul class="place-name"></ul>');
-   		  var $p= $('<p class="h5"></p>');
-   		  var $lat = $('<li class="lat"></li>');
-   		  var $lng = $('<li class="lng"></li>')
-   		  $p.html(value.name);
-   		  $lat.html(value.geometry.location.lat);
-   		  $lng.html(value.geometry.location.lng);
-	   		$ul.append($p);
-	   		$ul.append($lat);
-	   		$ul.append($lng);
-	   		$('#bus-name').append($ul);
-	   		$('#bus-name').append($ul);
-   	})
-     	
- });
+   getPlaceType("art_gallery");
 
    function getPlaceType(typeVal){
    	 $.post('place', { type: typeVal, field2 : "hello2"}, 
@@ -34,12 +14,15 @@ $.post('place', { type: "art_gallery", field2 : "hello2"},
    		  var $p= $('<p class="h5"></p>');
    		  var $lat = $('<li class="lat"></li>');
    		  var $lng = $('<li class="lng"></li>')
+   		  var $id = $('<li class="place-id"></li>')
    		  $p.html(value.name);
    		  $lat.html(value.geometry.location.lat);
    		  $lng.html(value.geometry.location.lng);
+   		  $id.html(value.place_id);
 	   		$ul.append($p);
 	   		$ul.append($lat);
 	   		$ul.append($lng);
+	   		$ul.append($id);
 	   		$('#bus-name').append($ul);
 	   		$('#bus-name').append($ul);
    	})
@@ -69,13 +52,27 @@ $(document).on('click', '.place-name', getLocation);
     var pName  = thisChildren[0].innerHTML;
 	  var latVal = thisChildren[1].innerHTML;
 	  var lngVal = thisChildren[2].innerHTML;
+	  var idVal = thisChildren[3].innerHTML;
+	  console.log(idVal);
 
 			 var mapOptions = {
     			center: new google.maps.LatLng(latVal,lngVal),
-    			zoom: 10,
+    			zoom: 11,
    		  	mapTypeId: google.maps.MapTypeId.ROADMAP
 				};
-			
+
+				var request = {
+				  placeId: idVal
+				};
+
+				var service = new google.maps.places.PlacesService(map);
+				service.getDetails(request, callback);
+
+				function callback(place, status) {
+					console.log(place);
+				  if (status == google.maps.places.PlacesServiceStatus.OK) {
+				//createMarker(place);
+
 			var map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
 			var markerOptions = {
@@ -83,13 +80,18 @@ $(document).on('click', '.place-name', getLocation);
 			};
 			var marker = new google.maps.Marker(markerOptions);
 			marker.setMap(map);
+
 			var infoWindowOptions = {
-    	content: pName
+    	content: place.formatted_address + '<br><br>' + place.formatted_phone_number
 			};
-		var infoWindow = new google.maps.InfoWindow(infoWindowOptions);
-		google.maps.event.addListener(marker,'click',function(e){
-  		infoWindow.open(map, marker);
-		});
+			var infoWindow = new google.maps.InfoWindow(infoWindowOptions);
+			google.maps.event.addListener(marker,'click',function(e){
+	  		infoWindow.open(map, marker);
+			});
+		  }
+	}
+
+			
 		
 };
 
