a212b714f3068067125228aaa5152a27e9cfc848
Noah Finnerman <nonothetoad@gmail.com>
2016-05-09 13:02:50 -0400

    remove autocomplete, add location to main input

--- public/js/app.js
+++ public/js/app.js
@@ -172,68 +172,70 @@ $(document).on('click', '.place-name', getLocation);
   });
    
 
-  $('.send-btn').on('click', function() {
-    var $location = $('#location-input').val();
-     $('#location-input').html(' ');
-    codeLatLng($location, function(addr){
-      
-      var mapOptions = {
-         center: new google.maps.LatLng(addr.intLat,addr.intLng),
-         zoom: 12,
-       mapTypeId: google.maps.MapTypeId.ROADMAP
-      };
-      var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-      var markerOptions = {
-      position: new google.maps.LatLng(addr.intLat, addr.intLng)
-      };
-      var marker = new google.maps.Marker(markerOptions);
-      marker.setMap(map);      
+  $('#autocomplete').change(function(){
 
-      var lat;
-      var lng;
-      var typeVal = "art_gallery"
-      if ( addr === undefined) {
-       lat = 41.65053;
-       lng = -73.932648;
-      } else { // here is where we update currentLocation data
-       lat = addr.intLat
-       lng = addr.intLng;
-       currentLocation.lat = addr.intLat;
-       currentLocation.lng = addr.intLng;
+      var $location = $('#autocomplete').val();
+       $('#location-input').html(' ');
+      codeLatLng($location, function(addr){
+        
+        var mapOptions = {
+           center: new google.maps.LatLng(addr.intLat,addr.intLng),
+           zoom: 12,
+         mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+        var markerOptions = {
+        position: new google.maps.LatLng(addr.intLat, addr.intLng)
+        };
+        var marker = new google.maps.Marker(markerOptions);
+        marker.setMap(map);      
 
-      }
-      
-      $.post('place', { type: typeVal, lat: lat,  lng: lng}, 
-        function(json){
-          $(".place-name").remove();
-          placeData = [];
-          $.each(json.results, function( index, value ) {
-          var $ul = $('<ul class="place-name"></ul>');
-          var $p= $('<p class="h5"></p>');
-          var $lat = $('<li class="lat"></li>');
-          var $lng = $('<li class="lng"></li>')
-          var $id = $('<li class="place-id"></li>')
-          var $photo_id = $('<li class="photo-id"></li>')
-          $p.html(value.name);
-          if (value.photos != undefined){
-             $photo_id.html(value.photos[0].photo_reference)
-          }
-          placeData[index] = {name: value.name, id: value.place_id};
-          $lat.html(value.geometry.location.lat);
-          $lng.html(value.geometry.location.lng);
-          $id.html(value.place_id);
-          $ul.append($p);
-          $ul.append($lat);
-          $ul.append($lng);
-          $ul.append($id);
-          $ul.append($photo_id);
-          $('#bus-name').append($ul);
+        var lat;
+        var lng;
+        var typeVal = "art_gallery"
+        if ( addr === undefined) {
+         lat = 41.65053;
+         lng = -73.932648;
+        } else { // here is where we update currentLocation data
+         lat = addr.intLat
+         lng = addr.intLng;
+         currentLocation.lat = addr.intLat;
+         currentLocation.lng = addr.intLng;
 
-          })
-         addCsvButton(); 
-        });
+        }
+        
+        $.post('place', { type: typeVal, lat: lat,  lng: lng}, 
+          function(json){
+            $(".place-name").remove();
+            placeData = [];
+            $.each(json.results, function( index, value ) {
+            var $ul = $('<ul class="place-name"></ul>');
+            var $p= $('<p class="h5"></p>');
+            var $lat = $('<li class="lat"></li>');
+            var $lng = $('<li class="lng"></li>')
+            var $id = $('<li class="place-id"></li>')
+            var $photo_id = $('<li class="photo-id"></li>')
+            $p.html(value.name);
+            if (value.photos != undefined){
+               $photo_id.html(value.photos[0].photo_reference)
+            }
+            placeData[index] = {name: value.name, id: value.place_id};
+            $lat.html(value.geometry.location.lat);
+            $lng.html(value.geometry.location.lng);
+            $id.html(value.place_id);
+            $ul.append($p);
+            $ul.append($lat);
+            $ul.append($lng);
+            $ul.append($id);
+            $ul.append($photo_id);
+            $('#bus-name').append($ul);
 
-      })
+            })
+           addCsvButton(); 
+          });
+
+        })
+    
    });
  
  function addCsvButton() {
@@ -400,55 +402,12 @@ function doRadarSearch(keyWords) {
   }
 }
 
-  $('.change-search').on('click', function(e){
+  $('.send-btn').on('click', function(e){
       e.preventDefault();
      var $string = $('#location-input').val();
-     console.log($string);
      doRadarSearch($string );
   });
 
    
-
-//code for auto complete input box to get a place.
-$('#autocomplete').focus(function(){
-
-  var mapOptions = {
-         center: new google.maps.LatLng(41.69463200000001, -73.935952),
-         zoom: 12,
-         mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-      
-  var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-  var acOptions = {
-    types: ['establishment']
-  };
-  var autocomplete = new google.maps.places.Autocomplete(document.getElementById('autocomplete'),acOptions);
-  autocomplete.bindTo('bounds',map);
-  var infoWindow = new google.maps.InfoWindow();
-  var marker = new google.maps.Marker({
-    map: map
-  });
-
-  google.maps.event.addListener(autocomplete, 'place_changed', function() {
-  	var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-    infoWindow.close();
-    var place = autocomplete.getPlace();
-    if (place.geometry.viewport) {
-      map.fitBounds(place.geometry.viewport);
-    } else {
-      map.setCenter(place.geometry.location);
-      map.setZoom(17);
-    }
-    marker.setPosition(place.geometry.location);
-    infoWindow.setContent('<div><strong>' + place.name + '</strong><br>' +'<a href="tel:+' +place.formatted_phone_number +'">'+ place.formatted_phone_number + '</a><br>');
-    infoWindow.open(map, marker);
-    google.maps.event.addListener(marker,'click',function(e){
-
-      infoWindow.open(map, marker);
-    });
-  });
-});
-
 
 })();
